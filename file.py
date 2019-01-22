890016a7df12c2074d49c1a81849cf728851adfc
pharaohandtrasher <44697305+pharaohandtrasher@users.noreply.github.com>
2019-01-22 18:47:29 +0500

    Update file.py

--- file.py
+++ file.py
@@ -1,11 +1,21 @@
 import os
 import tempfile
+
+"""
+You can use that class to write data to the file, sum files and iterate within strings in the file
+"""
+
+
 class File():
 	def __init__(self, filedirectory):
-		self.filedirectory=filedirectory
-		self.current=0
+		self.filedirectory = filedirectory
+		self.current = 0
+		
+		
 	def __str__(self):
 		return self.filedirectory
+	
+	
 	def write(self,text):
 		"""
 		Function that write value to the file
@@ -13,6 +23,8 @@ class File():
 		with open(self.filedirectory, 'w') as f:
 			f.write(text)
 	def __add__(self1,self2):
+		
+		
 		"""
 		You can use that function to get new 
 		file that unite two files and create
@@ -26,14 +38,20 @@ class File():
 					for line in f3:
 						f.write(line)
 					return File(os.path.join(tempfile.gettempdir(),'new_obj.txt'))
+				
+				
 	def _strings_count(self):
-		counter=0
+		counter = 0
 		with open(self.filedirectory, 'r') as f:
 			for line in f:
-				counter+=1
+				counter += 1
 			return counter
+		
+		
 	def __iter__(self):
 		return self
+	
+	
 	def __next__(self):
 		"""
 		Iterator which you can use to iterate
@@ -41,9 +59,9 @@ class File():
 		class)
 		"""
 		with open(self.filedirectory, 'r') as f:
-			lines=f.readlines()
-			if self.current<self._strings_count():
-				self.current+=1
+			lines = f.readlines()
+			if self.current < self._strings_count():
+				self.current += 1
 				return lines[int(self.current)-1]
 			else:
 				raise StopIteration
